3ea3134d9b7c85996f32479f31318f36e05a0eb3
fqhkhwrzm <muhammadfaqihk16@gmail.com>
2024-12-11 12:52:06 +0700

    mencoba token

--- src/server/handler.js
+++ src/server/handler.js
@@ -9,9 +9,16 @@ async function postPredictHandler(request, h) {
     const id = crypto.randomUUID();
     const createdAt = new Date().toISOString();
     const predictions_final = await predictMentalHealth(sentence);
+    const { authorization } = request.headers;
+    const token = authorization.replace("Bearer ", "")
+
+    if (!authorization) {
+        throw new InputError("Authorization token is required");
+    }
 
     const data = {
         id: id,
+        token: token,
         result: predictions_final,
         story: sentence,
         createdAt: createdAt
@@ -31,20 +38,28 @@ async function postPredictHandler(request, h) {
 async function predictionHistories(request, h) {
     try {
         const result = [];
-
         const histories = await fetchData();
+        const { authorization } = request.headers;
+        const token = authorization.replace("Bearer ", "");
+
+        if (!authorization) {
+            throw new InputError("Authorization token is required");
+        }
 
         histories.forEach((snapshot) => {
             const data = snapshot.data();
-            result.push({
-                id: snapshot.id,
-                history: {
-                    id: data.id,
-                    result: data.result,
-                    story: data.story,
-                    createdAt: data.createdAt
-                },
-            });
+
+            if (data.token === token) {
+                result.push({
+                    id: snapshot.id,
+                    history: {
+                        id: data.id,
+                        result: data.result,
+                        story: data.story,
+                        createdAt: data.createdAt
+                    },
+                });
+            }
         });
         const response = h.response({
             status: 'success',
